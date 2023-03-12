a60bd7bde4ea9eea2235ce0e93c654e9764b94fe
Seikendojo <94020124+Seikendojo@users.noreply.github.com>
2023-03-11 19:10:51 -0800

    Initial Commit

---
+++ LoanCalculatorUITests/LoanCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoanCalculatorUITestsLaunchTests.swift
+//  LoanCalculatorUITests
+//
+//  Created by Seiken Dojo on 2023-03-11.
+//
+
+import XCTest
+
+final class LoanCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
